d603570da51b74482d1e08551d417f704101b951
leejaechan <verb3511@gmail.com>
2025-01-21 12:14:24 +0900

    컴퍼넌트 비동기 작업

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import Home from './pages/Home';
-import Today from './pages/Today';
-import Developer from './pages/Developer';
-import Webd from './pages/Webd';
-import Website from './pages/Website';
-import Gsap from './pages/Gsap';
-import Port from './pages/Port';
-import Youtube from './pages/Youtube';
-import Channel from './pages/Channel';
-import Video from './pages/Video';
-import Search from './pages/Search';
-import Not from './pages/Not';
-import Header from './components/section/Header';
 import Main from './components/section/Main';
-import Footer from './components/section/Footer';
+
+// lazy() -> 컴포넌트를 동적으로 로드
+// 즉, 필요할 때에만 해당 컴포넌트의 코드를 로드하여 초기 로딩 시간을 줄임
+// 애플리케이션 성능 개선
+// lazy()와 Suspense는 함께 사용해야 올바르게 동작하며, 사용자 경험을 향상시킬 수 있습니다.
+const Home = lazy(() => import('./pages/Home'));
+const Today = lazy(() => import('./pages/Today'));
+const Developer = lazy(() => import('./pages/Developer'));
+const Webd = lazy(() => import('./pages/Webd'));
+const Website = lazy(() => import('./pages/Website'));
+const Gsap = lazy(() => import('./pages/Gsap'));
+const Port = lazy(() => import('./pages/Port'));
+const Youtube = lazy(() => import('./pages/Youtube'));
+const Channel = lazy(() => import('./pages/Channel'));
+const Video = lazy(() => import('./pages/Video'));
+const Search = lazy(() => import('./pages/Search'));
+const Not = lazy(() => import('./pages/Not'));
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Main>
+      <Suspense fallback={<Main />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/today" element={<Today />} />
@@ -36,8 +37,7 @@ const App = () => {
           <Route path="/search/:searchID" element={<Search />} />
           <Route path="/*" element={<Not />} />
         </Routes>
-      </Main>
-      <Footer />
+      </Suspense>
     </BrowserRouter>
   );
 };
