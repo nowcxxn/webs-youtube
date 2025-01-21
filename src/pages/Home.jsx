370f32a8b878a9a605ad4e2685a9da1b7d4d5e92
leejaechan <verb3511@gmail.com>
2025-01-21 21:27:54 +0900

    페이지 SEO 작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import Main from '../components/section/Main';
 
 const Home = () => {
-  return <Main>Home</Main>;
+  return (
+    <Main
+      title="이재찬 유튜브"
+      description="이재찬 유튜브에 오신것을 환영합니다."
+    >
+      Home
+    </Main>
+  );
 };
 
 export default Home;
